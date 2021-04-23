c1cac7be42b4e7f228cf167d248b2ea1bcd009a6
oswandor <oswandor26@gmail.com>
2021-04-22 20:30:41 -0600

    update

--- main.py
+++ main.py
@@ -3,11 +3,22 @@
 import os ; 
 import sys ; 
 import io ; 
+from distutils.spawn import find_executable
+from src.Capturing.recording import Capturing_audio , Playaudio; 
 
-from src.Capturing.recording import *;  
+try: 
+    Capturing =  Capturing_audio()
+    Capturing.searchlib() 
+    print(Capturing.screencast('nuevoscreen.mkv' ,'00:01:00','1280x1024' ,':1.0',aspectarg='4:3')); 
+    
+except Exception as e: 
+    print(e); 
 
-Capturing =  Capturing_audio(); 
+#method recordAudio 
+#Capturing.recordAudio('recording0.m4a', '00:01:00').stdout; 
 
+#play audio class Playaudio 
+#print(Playaudio.play('recording0.m4a')); 
 
 
 
--- src/Capturing/recording.py
+++ src/Capturing/recording.py
@@ -1,29 +1,54 @@
 #!/usr/bin/python3
-import subprocess; 
+import subprocess ; 
+from distutils.spawn import find_executable
+
+class Capturing_audio:
+
+    def searchlib(self):
+
+        foundffmepg = find_executable('ffmpeg') is not None ;
+        foundffplay = find_executable('ffplay')  is not None;
+        foundpactl = find_executable('pactl') is not None ;
+        
+        if foundffmepg and foundffplay and foundpactl :
+           print("All good..") 
+        else : 
+          raise Exception('Error: Some of the required commands were not found ffmpeg, ffplay or pactl.');
+          exit(0); 
 
 
-class Capturing_audio():
-   
-    def __init__(self):
-        """Get information `pactl list short sources`"""
-           
     def getinput_devices(self): 
 
-        try:
-         p  = subprocess.run(['pactl', 'list' ,'short' ,'sources'] , capture_output=True ); 
-         return self.encodeutf(p.stdout).split('\t')[1]; 
+        found = find_executable('pactl') is not None 
+        if found:
+            try:
+                p  = subprocess.run(['pactl', 'list' ,'short' ,'sources'] , capture_output=True ); 
+                return self.encodeutf(p.stdout).split('\t')[1]; 
 
-        except: 
-            pass 
-    def recoringaudio(self,timesecoss):
+            except Exception as e: 
+                print('Faild ' + e)
+
+# ffmpeg -f x11grab -video_size 1280x1024  -framerate 30 -i :1.0 -vcodec libx264  -preset ultrafast -qp 0  -pix_fmt yuv420p -aspect 2:3 23escale.mkv 
+
+    def  screencast(self,filenameout , timesec , resolution  , display ,  aspectarg='16:9'):
+
+        try: 
+            p = subprocess.run(['ffmpeg','-ss','00:00:00','-t', timesec ,'-f', 'x11grab', '-video_size' , resolution , '-framerate', '30', '-i', display ,'-vcodec', 'libx264'  ,'-preset' ,'ultrafast' ,'-qp' ,'0' , '-pix_fmt', 'yuv420p' ,'-aspect', aspectarg ,filenameout ])
+            return p.stdout 
+        except Exception as e: 
+            return e 
+
+
+
+
+
+    def recordAudio(self,nameRecoding, timesec):
 
         try:
-            p =  subprocess.run(['ffmpeg', '-f' ,'pulse', '-i' , DEVICEPULSARAUDIO , '-ac', '1','recording.m4a']);
+            p =  subprocess.run(['ffmpeg', '-ss' ,'00:00:00' , '-t' , timesec ,  '-f' ,'pulse', '-i' , DEVICEPULSARAUDIO , '-ac', '1', nameRecoding]);
             return p ; 
-        except:
-            pass
-
-
+        except Exception as e: 
+            print('Faild ' + e)
 
 
     def encodeutf(self , bytess):
@@ -36,3 +61,17 @@ class Capturing_audio():
 
 DEVICEPULSARAUDIO = Capturing_audio().getinput_devices();
 
+
+class Playaudio():
+
+    def play(namefile):
+
+        try:
+            process = subprocess.run(['ffplay' , namefile] );
+
+            if  process.returncode == 0 : 
+                 raise  Exception("Error file does not exist.") ; 
+
+            return process 
+        except Exception as e : 
+            return e 
